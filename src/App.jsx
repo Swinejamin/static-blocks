15fc5794edb101aff7bc118f4278134c0bdaafa4
Benjamin <benjamin@swineford.me>
2018-06-27 21:20:30 -0500

    ah god why it's so close

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
 import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
+import { Link, Router, Route, Switch } from 'react-static'
 //
-import app from './app.blocks.css'
-import logo from './logo.png'
+import styles from './App.block.css'
+
+// import logo from './logo.png'
 
 class App extends Component {
   render () {
     return (
-      <div className={app} >
-        <header className="App-header" >
-          <img src={logo} className="App-logo" alt="logo" />
-          {/* eslint-disable */}
-          <h1 className="App-title" >Welcome to React-Static</h1 >
-          {/* eslint-enable */}
+      <Router className={styles.app} >
+        <header className={styles.appHeader} >
+          <ul >
+            <li ><Link to="/About" >About</Link ></li >
+            <li ><Link to="/Home" >Home</Link ></li >
+          </ul >
         </header >
-        <p className="App-intro" >
-          To get started, edit <code >src/App.js</code > and save to reload.
-        </p >
-      </div >)
+        <Switch >
+
+          <Route path="/About" render={({ match }) => {
+            console.log(match)
+            return (
+              <p className="App-intro" >
+                To get started, edit <code >src/App.js</code > and save to reload.
+                <pre >{match}</pre >
+              </p >)
+          }} />
+          <Route path="/Home" >
+            <p className="App-intro" >
+              Home page, bitch
+
+            </p >
+          </Route >
+        </Switch >
+      </Router >)
   }
 }
 
