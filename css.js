aa5f34e36d303a4782888e891e5a28577c88dded
Benjamin <benjamin@swineford.me>
2018-07-24 22:21:11 -0500

    probably gonna have to revert this one...

--- css.js
+++ css.js
@@ -3,48 +3,53 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var cssBlocksWebpackPlugin = require("@css-blocks/webpack");
 var webpackSources = require("webpack-sources");
 // tslint:disable-next-line:no-var-requires
-var CleanCSS = require('clean-css');
+var CleanCSS = require("clean-css");
 // tslint:disable-next-line:no-var-requires
-var autoprefixer = require('autoprefixer');
+var autoprefixer = require("autoprefixer");
 var minifier = new CleanCSS({
   returnPromise: true,
   level: 1,
   sourceMap: true,
-  sourceMapInlineSources: false,
+  sourceMapInlineSources: false
 });
-var noopProcessor = function (asset) { return asset; };
-var minifierAdapter = function (asset, assetPath) {
+var noopProcessor = function(asset) {
+  return asset;
+};
+var minifierAdapter = function(asset, assetPath) {
   var source = asset.source();
   var map = asset.map();
-  return minifier.minify(source).then(function (result) {
+  return minifier.minify(source).then(function(result) {
     // console.log(`${assetPath} minified from ${result.stats.originalSize} to ${result.stats.minifiedSize} in ${
     //     result.stats.timeSpent
     //   }ms`,
     // );
     var g = result.sourceMap;
-    return new webpackSources.SourceMapSource(result.styles, assetPath, g.toJSON(), source, map);
+    return new webpackSources.SourceMapSource(
+      result.styles,
+      assetPath,
+      g.toJSON(),
+      source,
+      map
+    );
   });
 };
-module.exports = function (options) {
-  let browsers = options.browsers || [
-    "> 1%",
-    "last 2 versions"
-  ];
+module.exports = function(options) {
+  let browsers = options.browsers || ["> 1%", "last 2 versions"];
   var assetOptions = {
     cssFiles: {
-      'prism.css': 'public/lib/prismjs/prism.css',
-      'baseline.css': 'public/lib/baseline.css',
+      "prism.css": "public/lib/prismjs/prism.css",
+      "baseline.css": "public/lib/baseline.css"
     },
     concat: {
-      'css-blocks.css': {
-        sources: ['prism.css', 'baseline.css', 'blocks.css'],
+      "css-blocks.css": {
+        sources: ["prism.css", "baseline.css", "blocks.css"],
         postProcess: {
           postcss: [autoprefixer({ browsers })],
-          processor: options.minify ? minifierAdapter : noopProcessor,
-        },
-      },
+          processor: options.minify ? minifierAdapter : noopProcessor
+        }
+      }
     },
-    inlineSourceMaps: options.inlineSourceMaps,
+    inlineSourceMaps: options.inlineSourceMaps
   };
   return new cssBlocksWebpackPlugin.CssAssets(assetOptions);
 };
--- static.config.js
+++ static.config.js
@@ -1,12 +1,14 @@
-// Though this file is optional, there are SO MANY COOL THINGS you can do here.
-// Read the docs at https://github.com/nozzle/react-static/blob/master/README.md to learn more!
-
 import axios from "axios";
 import cssAssets from "./css";
-
-const paths = require("./paths");
-
-const eslintFormatter = require("react-dev-utils/eslintFormatter");
+const cssFilename = "static/css/[name].[contenthash:8].css";
+import autoprefixer from "autoprefixer";
+import paths from "./paths";
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const publicPath = paths.servedPath;
+const shouldUseRelativeAssetPaths = publicPath === "./";
+const extractTextPluginOptions = shouldUseRelativeAssetPaths // Making sure that the publicPath goes back to to build folder.
+  ? { publicPath: Array(cssFilename.split("/").length).join("../") }
+  : {};
 const jsxCompilationOptions = {
   compilationOptions: {},
   aliases: {},
@@ -18,11 +20,13 @@ const jsxCompilationOptions = {
     enabled: true
   }
 };
-const CssBlocks = require("@css-blocks/jsx");
-const CssBlocksPlugin = require("@css-blocks/webpack").CssBlocksPlugin;
-const CssBlockRewriter = new CssBlocks.Rewriter();
-const CssBlockAnalyzer = new CssBlocks.Analyzer(
-  "unique-name",
+
+const { Rewriter, Analyzer } = require("@css-blocks/jsx");
+const { CssBlocksPlugin } = require("@css-blocks/webpack");
+const cssBlocksRewriter = require("@css-blocks/jsx/dist/src/transformer/babel");
+const rewriter = new Rewriter();
+const analyzer = new Analyzer(
+  __dirname + "/src/index.jsx",
   jsxCompilationOptions
 );
 
@@ -61,16 +65,23 @@ export default {
     ];
   },
   webpack: (config, { defaultLoaders, stage }) => {
+    config.entry = `${__dirname}/src/index.jsx`;
     config.plugins.push(
       new CssBlocksPlugin({
-        analyzer: CssBlockAnalyzer,
+        analyzer: analyzer,
         outputCssFile: "blocks.css",
         name: "css-blocks",
         compilationOptions: {},
         optimization: {}
-      }) // cssAssets({minify: true, inlineSourceMaps: false})
+      }),
+      new ExtractTextPlugin({
+        // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
+        filename: cssFilename
+      })
     );
     config.resolve.extensions = [
+      ".scss",
+      ".css",
       ".web.js",
       ".mjs",
       ".js",
@@ -80,100 +91,107 @@ export default {
       ".ts",
       ".tsx"
     ];
+    // config.devtool= shouldUseSourceMap ? 'source-map' : false,
+
+    // This allows you to set a fallback for where Webpack should look for modules.
+    // We placed these paths second because we want `node_modules` to "win"
+    // if there are any conflicts. This matches Node resolution mechanism.
+    // https://github.com/facebookincubator/create-react-app/issues/253
+
+    // config.resolve.modules = ["node_modules", paths.appNodeModules].concat(
+    //   // It is guaranteed to exist because we tweak it in `env.js`
+    //   process.env.NODE_PATH.split(path.delimiter).filter(Boolean)
+    // );
+
     config.module.rules = [
-      // First, run the linter.
-      // It's important to do this before Babel processes the JS.
-      {
-        test: /\.(js|jsx|mjs)$/,
-        enforce: "pre",
-        use: [
-          {
-            options: {
-              formatter: eslintFormatter,
-              eslintPath: require.resolve("eslint")
-            },
-            loader: require.resolve("eslint-loader")
-          }
-        ],
-        include: paths.appSrc
-      },
       {
         // "oneOf" will traverse all following loaders until one will
         // match the requirements. When no loader matches it will fall
         // back to the "file" loader at the end of the loader list.
-        oneOf: [
-          {
-            test: /\.[j|t]s(x?)$/,
-            exclude: /node_modules/,
-            use: [
-              {
-                loader: require.resolve("babel-loader"),
-                options: {
-                  presets: [require.resolve("babel-preset-react-app")],
-                  cacheDirectory: true,
-                  compact: true
-                }
-              },
-
-              // Run the css-blocks plugin in its own dedicated loader because the react-app preset
-              // steps on our transforms' feet. This way, we are guaranteed a clean pass before any
-              // other transforms are done.
-              {
-                loader: require.resolve("babel-loader"),
-                options: {
-                  plugins: [
-                    require("@css-blocks/jsx/dist/src/transformer/babel").makePlugin(
-                      { rewriter: CssBlockRewriter }
-                    )
-                  ],
-                  cacheDirectory: true,
-                  compact: true,
-                  parserOpts: {
-                    plugins: [
-                      "jsx",
-                      "doExpressions",
-                      "objectRestSpread",
-                      "decorators",
-                      "classProperties"
-                    ]
-                  }
-                }
-              },
-
-              // The JSX Webpack Loader halts loader execution until after all blocks have
-              // been compiled and template analyses has been run. StyleMapping data stored
-              // in shared `rewriter` object.
-              {
-                loader: require.resolve("@css-blocks/webpack/dist/src/loader"),
-                options: {
-                  analyzer: CssBlockAnalyzer,
-                  rewriter: CssBlockRewriter
-                }
-              }
-            ]
-          }, // defaultLoaders.cssLoader,
-          // defaultLoaders.jsLoader,
-          // defaultLoaders.fileLoader,
-
-          // "file" loader makes sure those assets get served by WebpackDevServer.
-          // When you `import` an asset, you get its (virtual) filename.
-          // In production, they would get copied to the `build` folder.
-          // This loader doesn't use a "test" so it will catch all modules
-          // that fall through the other loaders.
-          {
-            // Exclude `js` files to keep "css" loader working as it injects
-            // it's runtime that would otherwise processed through "file" loader.
-            // Also exclude `html` and `json` extensions so they get processed
-            // by webpacks internal loaders.
-            exclude: [/\.[j|t]s(x?)$/, /\.html$/, /\.json$/],
-            loader: require.resolve("file-loader"),
-            options: {
-              name: "static/media/[name].[hash:8].[ext]"
-            }
-          }
-        ]
-      } // ** STOP ** Are you adding a new loader?
-      // Make sure to add the new loader(s) before the "file" loader.
-    ];
+        // oneOf: [
+        // defaultLoaders.jsLoader,
+        // defaultLoaders.fileLoader,
+        // defaultLoaders.cssLoader,
+        // {
+        //   test: /\.(s?)css$/,
+        //   loader: ExtractTextPlugin.extract(
+        //     Object.assign(
+        //       {
+        //         use: [
+        //           "css-loader",
+        //           {
+        //             loader: require.resolve("postcss-loader"),
+        //             options: {
+        //               // Necessary for external CSS imports to work
+        //               // https://github.com/facebookincubator/create-react-app/issues/2677
+        //               ident: "postcss",
+        //               plugins: () => [
+        //                 require("postcss-flexbugs-fixes"),
+        //                 autoprefixer({
+        //                   browsers: [
+        //                     ">1%",
+        //                     "last 4 versions",
+        //                     "Firefox ESR",
+        //                     "not ie < 9" // React doesn't support IE8 anyway
+        //                   ],
+        //                   flexbox: "no-2009"
+        //                 })
+        //               ]
+        //             }
+        //           }
+        //         ]
+        //       },
+        //       extractTextPluginOptions
+        //     )
+        //   )
+        // },
+        // {
+        //   test: /\.[j|t]s(x?)$/,
+        //   exclude: /node_modules/,
+        //   use: [
+        //     {
+        //       loader: require.resolve("babel-loader"),
+        //       options: {
+        //         presets: ["env", "react", "stage-2"],
+        //         cacheDirectory: true,
+        //         compact: true
+        //       }
+        //     },
+        //     {
+        //       loader: require.resolve("babel-loader"),
+        //       options: {
+        //         plugins: [cssBlocksRewriter.makePlugin({ rewriter })],
+        //         parserOpts: {
+        //           plugins: ["jsx"]
+        //         }
+        //       }
+        //     },
+        //     {
+        //       loader: require.resolve("@css-blocks/webpack/dist/src/loader"),
+        //       options: {
+        //         analyzer,
+        //         rewriter
+        //       }
+        //     }
+        //   ]
+        // },
+        // "file" loader makes sure those assets get served by WebpackDevServer.
+        // When you `import` an asset, you get its (virtual) filename.
+        // In production, they would get copied to the `build` folder.
+        // This loader doesn't use a "test" so it will catch all modules
+        // that fall through the other loaders.
+        // {
+        //   // Exclude `js` files to keep "css" loader working as it injects
+        //   // it's runtime that would otherwise processed through "file" loader.
+        //   // Also exclude `html` and `json` extensions so they get processed
+        //   // by webpacks internal loaders.
+        //   exclude: [/\.[j|t]s(x?)$/, /\.html$/, /\.json$/],
+        //   loader: require.resolve("file-loader"),
+        //   options: { name: "static/media/[name].[hash:8].[ext]" }
+        // }
+        // ]
+      }
+    ]; // ** STOP ** Are you adding a new loader?
+    // Make sure to add the new loader(s) before the "file" loader.
   }
 };
